be410d1f2c07647f7f11fcf5eed1f7a23b6917d9
shevyakhov <shevyakhov89@gmail.com>
2021-11-05 16:59:48 +0700

    CartFragment.kt layout

--- app/src/main/java/fragments/CartFragment.kt
+++ app/src/main/java/fragments/CartFragment.kt
@@ -16,7 +16,7 @@ class CartFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         binding = FragmentCartBinding.inflate(inflater)
         return binding.root
     }
